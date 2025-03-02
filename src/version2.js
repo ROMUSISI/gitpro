5118bf9d388c91f528965ebe62fe84cc660f222e
Musisi <musisironald2025@gmail.com>
2025-03-02 16:01:20 +0300

    Version 1

---
+++ src/version2.js
@@ -0,0 +1,4 @@
+console.log ('Hello world');
+console.log ('Learning git');
+
+console.log ('this is version 4')
---
+++ version1.js
@@ -0,0 +1,6 @@
+console.log ('Hello world');
+console.log ('Learning git');
+console.log ('hello guys');
+const index = 1;
+
+console.log('This is version 3')
